ae6e2a181ef1b84e61e70c46f396462e27725261
Arup Dutta <dutta.arup59@gmail.com>
2022-05-08 18:25:00 +0530

    BuilderDp

---
+++ src/com/arup/dp/creationalDesignPatterns/builder/BuilderDpRunner.java
@@ -0,0 +1,12 @@
+package com.arup.dp.creationalDesignPatterns.builder;
+
+public class BuilderDpRunner {
+
+	public static void main(String[] args) {
+		Vehicle car = new Vehicle.VehicleBuilder("1800cc",4).setAirbag(true).build();
+		System.out.println(car);
+		Vehicle bike = new Vehicle.VehicleBuilder("500cc", 2).build();
+		System.out.println(bike);
+	}
+
+}
---
+++ src/com/arup/dp/creationalDesignPatterns/builder/Vehicle.java
@@ -0,0 +1,50 @@
+package com.arup.dp.creationalDesignPatterns.builder;
+
+public class Vehicle {
+	
+	//Necessary feature
+	int noOfWheels;
+	String engine;
+	
+	//Optional Feature
+	boolean airbag;
+	
+	private Vehicle(String engine, int noOfWheels){
+		this.engine = engine;
+		this.noOfWheels = noOfWheels;
+	}
+	
+	@Override
+	public String toString() {
+		return "Vehicle [noOfWheels=" + noOfWheels + ", engine=" + engine + ", airbag=" + airbag + "]";
+	}
+
+
+
+	private void setAirbag(boolean airbag){
+		this.airbag = airbag;
+	}
+	
+	static class VehicleBuilder{
+		int noOfWheels;
+		String engine;
+		boolean airbag;
+		
+		VehicleBuilder(String engine, int noOfWheels){
+			this.engine = engine;
+			this.noOfWheels = noOfWheels;
+		}
+		
+		public VehicleBuilder setAirbag(boolean airbag){
+			this.airbag = airbag;
+			return this;
+		}
+		
+		public Vehicle build(){
+			Vehicle v = new Vehicle(this.engine, this.noOfWheels);
+			v.setAirbag(this.airbag);
+			return v;
+		}
+	}
+
+}
